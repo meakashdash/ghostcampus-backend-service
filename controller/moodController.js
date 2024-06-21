ba6d616e2e4b98742f9915ca08585169badf253b
meakashdash1 <akashdash279@gmail.com>
2024-06-22 01:42:43 +0530

    refactor: createMood api update functionality added

--- controller/moodController.js
+++ controller/moodController.js
@@ -5,70 +5,110 @@ import moment from "moment";
 
 const mongoDBService = new MongoDBService();
 
-export const createMood=async(req,res)=>{
-    try {
-        const {userId}=req.user;
-        const moodItem={
-            _id:new ObjectId(),
-            userId:userId,
-            date:moment().format('YYYY-MM-DD'),
-            mood:req.body.mood
-        }
-        const moodCreateResponse=await mongoDBService.createItem(
-            TABLE_NAMES.MOOD_CALENDER,
-            moodItem
-        )
-        if(!moodCreateResponse){
-            return res.json({
-                statusCode:401,
-                message:"Mood Creation Failed. Try Again"
-            })
-        }
-        return res.json({
-            statusCode:200,
-            message:"Mood Created Successfully"
-        })
-    } catch (error) {
+export const createMood = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    if(req.body.date>moment().format('YYYY-MM-DD')){
         return res.json({
             statusCode:400,
-            message:error.message
+            message:"Can't Post Mood for Future"
         })
     }
-}
+    const query = {
+      userId: userId,
+      date: req.body.date,
+    };
+    const moodResponse = await mongoDBService.findByQueryArray(
+      TABLE_NAMES.MOOD_CALENDER,
+      query
+    );
+    if (moodResponse.length !== 0) {
+      const mood = moodResponse[0];
+      const filter = { _id: new ObjectId(mood._id) };
+      const updateOperation = { $set: { mood: req.body.mood } };
+      const updateResponse = await mongoDBService.addItemQuery(
+        TABLE_NAMES.MOOD_CALENDER,
+        filter,
+        updateOperation
+      );
+      if (!updateResponse) {
+        return res.json({
+          statusCode: 404,
+          message: "Unable to update the mood. Please try again",
+        });
+      }
 
-export const getMoodPerMonth=async(req,res)=>{
-    try {
-        const {month,year}=req.query;
-        const {userId}=req.user;
-        if (!month || !year) {
-            return res.json({
-                statusCode: 400,
-                message: 'Month and year are required'
-            });
-        }
-        const monthInt=parseInt(month);
-        const yearInt=parseInt(year);
-        const startOfMonth = moment([yearInt, monthInt - 1]).startOf('month').format('YYYY-MM-DD');
-        const endOfMonth = moment([yearInt, monthInt - 1]).endOf('month').format('YYYY-MM-DD');
-        const query = {
-            userId: userId,
-            date: {
-                $gte: startOfMonth,
-                $lte: endOfMonth
-            }
-        };
-        const moods=await mongoDBService.findByQueryArray(
-            TABLE_NAMES.MOOD_CALENDER,
-            query
-        )
+      return res.json({
+        statusCode: 200,
+        message: "Mood Updated Successfully",
+      });
+    } else {
+      const moodItem = {
+        _id: new ObjectId(),
+        userId: userId,
+        date: moment().format("YYYY-MM-DD"),
+        mood: req.body.mood,
+      };
+      const moodCreateResponse = await mongoDBService.createItem(
+        TABLE_NAMES.MOOD_CALENDER,
+        moodItem
+      );
+      if (!moodCreateResponse) {
         return res.json({
-            statusCode:200,
-            moods
-        })
-    } catch (error) {
-        return res.json({
-            statusCode:400,
-            message:error.message
-        })
+          statusCode: 401,
+          message: "Mood Creation Failed. Try Again",
+        });
+      }
+      return res.json({
+        statusCode: 200,
+        message: "Mood Created Successfully",
+      });
     }
-}
+  } catch (error) {
+    return res.json({
+      statusCode: 400,
+      message: error.message,
+    });
+  }
+};
+
+export const getMoodPerMonth = async (req, res) => {
+  try {
+    const { month, year } = req.query;
+    const { userId } = req.user;
+    if (!month || !year) {
+      return res.json({
+        statusCode: 400,
+        message: "Month and year are required",
+      });
+    }
+    const monthInt = parseInt(month);
+    const yearInt = parseInt(year);
+    const startOfMonth = moment([yearInt, monthInt - 1])
+      .startOf("month")
+      .format("YYYY-MM-DD");
+    const endOfMonth = moment([yearInt, monthInt - 1])
+      .endOf("month")
+      .format("YYYY-MM-DD");
+    const query = {
+      userId: userId,
+      date: {
+        $gte: startOfMonth,
+        $lte: endOfMonth,
+      },
+    };
+    const moods = await mongoDBService.findByQueryArray(
+      TABLE_NAMES.MOOD_CALENDER,
+      query
+    );
+    return res.json({
+      statusCode: 200,
+      moods,
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: 400,
+      message: error.message,
+    });
+  }
+};
